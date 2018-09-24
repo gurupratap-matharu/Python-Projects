cfb26d09be00eb24e85e98a4e1bcd5fce9569927
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2018-09-24 15:11:21 -0300

    removing unnecessary files

--- Webmaps-using-python-folium/map.py
+++ Webmaps-using-python-folium/map.py
@@ -1,20 +1,34 @@
-import folium, pandas
-import webbrowser
+import folium
+import pandas
 
-data = pandas.read_csv("volcanoes.txt")
+data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
-long = list(data["LON"])
+lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="MapBox Control Room")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
-fg = folium.FeatureGroup("name=MyMap")
+for lt, ln, el in zip(lat, lon, elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m",
+    fill_color=color_producer(el), fill=True,  color = 'grey', fill_opacity=0.7))
 
-for lat, lon, elev in zip(lat, long, elev):
-	fg.add_child(folium.Marker(location=[lat,long], popup = str(elev), icon=folium.Icon(color="green")))
+fgp = folium.FeatureGroup(name="Population")
 
-map.add_child(fg)
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.save("Map.html")
-webbrowser.open("Map.html")
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
+map.save("Map1.html")
---
+++ interactive-dictionary/README.md
@@ -0,0 +1,16 @@
+# Command Line Interactive Dictionary
+
+A basic command line dictionary using python and a sqlite database.
+
+- users an sqlite database
+- uses an intelligent feature of recommending a similar word in case entered word is not in database
+- incase of more than one definitions displays each one on a new line.
+- always in a loop unless exited by Ctrl-C or blank space
+
+Write a word to find meaning to it.
+
+## Running the Dictionary
+
+- Python 3.4+: `python dictionary.py`
+    
+To exit simply press enter without typing a word.
